e3faaa0e591748aa4a3e3fc4b698a54e32f438e5
omarafifii <48727083+omarafifii@users.noreply.github.com>
2022-07-25 01:27:34 +0200

    update urls

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a493158fea5974b429f8a9348a46f909-297349922.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
